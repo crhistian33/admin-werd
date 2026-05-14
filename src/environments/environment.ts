24a3eb9294532ed656c0df2f9a4a2240c97c86f0
crhistian33 <crhistian.pj@gmail.com>
2026-05-14 00:40:47 -0500

    Configure files for build in Vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   useMocks: false, // ← nunca mocks en producción
-  apiUrl: 'https://api.tu-dominio.com',
-  apiImagesUrl: 'https://api.tu-dominio.com',
+  apiUrl: 'https://api-services-werd-production.up.railway.app/api/v1',
+  apiImagesUrl: 'https://api-services-werd-production.up.railway.app',
 };
